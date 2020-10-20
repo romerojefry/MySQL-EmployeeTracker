c87de9d9450f5226d7edce2627d615fa5d9de0f4
Jefry Romero <romerojefry@icloud.com>
2020-10-20 16:51:22 -0700

    creating starter tacle for mysql on schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS Employee_TrackerDB;
+
+CREATE DATABASE Employee_TrackerDB;
+
+USE Employee_TrackerDB;
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT,
+    employee_name VARCHAR(30),
+    role_id INTEGER (200),
+    manager_id INTEGER(10) NULL,
+);
