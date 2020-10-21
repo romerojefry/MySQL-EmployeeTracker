7d28e2b18bff577bff34fa6a7402baffdceb39c4
Jefry Romero <romerojefry@icloud.com>
2020-10-20 20:11:24 -0700

    started on working on prompt choices

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { start } = require("repl");
 
 var connection = mysql.createConnection({
-    host: "localhoest",
+    host: "localhost",
 
     port: 3306,
 
@@ -15,4 +16,27 @@ var connection = mysql.createConnection({
 connection.connect(function (err){
     if (err) throw err;
     console.log("connected as id" + connection.threadId + "\n");
-})
+    starter ()
+});
+function starter (){
+    inquirer
+    .prompt ([
+    {
+        type: "list",
+        message: "access options",
+        name: "options",
+        chocies: [
+            "Add Employee",
+            "Add Manger",
+            "Add Roles"
+         ]
+
+        }
+    ])
+    .then(function(res){
+        switch(res.start){
+            case "Add Employee":
+            addEmployee
+        }
+    })
+}
