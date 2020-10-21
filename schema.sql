f7b7715beef12efd0f582594d1d74fe59fd2e8ed
Jefry Romero <romerojefry@icloud.com>
2020-10-20 22:12:03 -0700

    adding tables department and roles to mysql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS Employee_TrackerDB;
 CREATE DATABASE Employee_TrackerDB;
 
 USE Employee_TrackerDB;
-
+--! creating my grid view for MySQL Workbench--
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT,
     employee_name VARCHAR(30),
@@ -11,11 +11,20 @@ CREATE TABLE employees (
     manager_id INTEGER(10) NULL,
     PRIMARY KEY (id)
 );
-CREATE TABLE Department (
+CREATE TABLE Roles (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     Pay DECIMAL (30),
+    department_id INT(10)
 
-)
+);
+CREATE TABLE Department (
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(30),
+    department_id INT(10)
+);
 
+--! connecting to seeds.sql where it has my values--
 SELECT * FROM employees; 
+SELECT * FROM Roles;
+SELECT * FROM Department;
