fc142c4e3b6eb4dd88b4867d2edd4c10146f404c
Jefry Romero <romerojefry@icloud.com>
2020-10-20 22:53:38 -0700

    adding primary key to schema

--- schema.sql
+++ schema.sql
@@ -8,20 +8,22 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT,
     employee_name VARCHAR(30),
     role_id INTEGER (10),
-    manager_id INTEGER(10) NULL,
+    department_id INTEGER(10) NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE Roles (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     Pay DECIMAL (30),
-    department_id INT(10)
+    department_id INT(10),
+    PRIMARY KEY (id)
 
 );
 CREATE TABLE Department (
     id INTEGER AUTO_INCREMENT,
     name VARCHAR(30),
     department_id INT(10)
+    PRIMARY (id)
 );
 
 --! connecting to seeds.sql where it has my values--
