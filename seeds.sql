98a2f04bc487f394d56e57539444cb18c9decf60
Jefry Romero <romerojefry@icloud.com>
2020-10-20 22:36:07 -0700

    adding values to seed for my sql workbench

--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,8 @@
-INSERT INTO employees (employee_name, role_id, manger_id)
-VALUES ("Songebob,", 1, 3);
+INSERT INTO employees (employee_name, role_id, department_id)
+VALUES ("Songebob", 1, 3); ("Squidward", 2, 2); ("Mr. Krabs", 3, 1);
+
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Cook",400, 3); ("Cashier", 450, 2);
+
+INSERT INTO Department (name, department_id)
+VALUES ("Cheif",3); ("Account", 2); ("Owner", 1);
